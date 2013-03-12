25ae64dfdadcdf4dcf013128e71586e435ae8d75
Julio Barros <Julio@E-String.com>
2013-03-11 22:32:05 -0700

    updated clojure and liblinear libs in project.clj, removed set-all! macro, added save-model and load-model

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject clj-liblinear "0.0.1-SNAPSHOT"
   :description "A Clojure wrapper for LIBLINEAR, a linear support vector machine library."
-  :dependencies [[clojure "1.2.0"]
-                 [de.bwaldvogel/liblinear "1.8"]]
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [de.bwaldvogel/liblinear "1.92"]]
   
   :dev-dependencies [[swank-clojure "1.3.2"]])
