16b7972400d9d21f24fd9d0deecc44325b336946
Jason Toy <jason@socmetrics.com>
2013-06-25 14:37:34 -0400

    version revert

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clj-liblinear "0.2.0"
+(defproject clj-liblinear "0.1.0"
   :description "A Clojure wrapper for LIBLINEAR, a linear support vector machine library."
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [de.bwaldvogel/liblinear "1.92"]])
