ed32a6850aea4e73f364bf32acb2b1f0c806366f
Kevin J. Lynagh <kevin@keminglabs.com>
2014-04-16 09:03:48 -0700

    Update liblinear to 1.94.
    Closes #7.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
 (defproject clj-liblinear "0.1.1-SNAPSHOT"
   :description "A Clojure wrapper for LIBLINEAR, a linear support vector machine library."
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [de.bwaldvogel/liblinear "1.92"]])
+                 [de.bwaldvogel/liblinear "1.94"]])
